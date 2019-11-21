9b854cae6061149d5199c6f92384564a44f312a6
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2019-11-21 08:50:07 -0800

    (feat) got the basic server routes going

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['fasdfsdf'] }));
 app.use(router);
 
 app.listen(PORT, () => {
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,13 +1,33 @@
 import { Router, Request, Response } from 'express';
 
-const router = Router();
+const router: Router = Router();
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
 
 router.get('/login', (req: Request, res: Response) => {
   res.send(`
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" type="email" placeholder="email address..." />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" placeholder="login password..." />
+      </div>
+      <button>Submit</button>
+    </form>
   `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) res.status(401).send('Please enter an email and password');
+
+  else res.send(email + password);
 });
 
 export { router };
